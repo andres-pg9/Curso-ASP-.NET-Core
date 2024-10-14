1ad525eb8dd5f0d6e4f3cad079a6fce0e3b0f5a5
andres-pg9 <115204261+andres-pg9@users.noreply.github.com>
2024-10-14 11:38:00 -0600

    Version inicial del proyecto

---
+++ AspNetCoreTodo/Controllers/TodoController.cs
@@ -0,0 +1,18 @@
+//Este controlador tendrá la funcionalidad de la lista de tareas
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreTodo.Controllers
+{
+    public class TodoController : Controller
+    {
+        public IActionResult Index()
+        {
+            
+        }
+    }
+}
